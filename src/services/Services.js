1cfc31d911978ee906ab78314b62b8f99e42079a
tiago-ro <tiago.ro.dev@gmail.com>
2024-02-07 12:07:09 -0300

    Finishing the CRUD

--- src/services/Services.js
+++ src/services/Services.js
@@ -10,6 +10,27 @@ class Services {
     
   }
 
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
+
+  async criaRegistro() {
+    return dataSource[this.model].create();
+  }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const listaDeRegistroAtualizado = dataSource[this.model].update(dadosAtualizados, {
+      where: { id: id }
+    });
+    if (listaDeRegistroAtualizado[0] === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  async excuiRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
+  }
 }
 
 module.exports = Services;
