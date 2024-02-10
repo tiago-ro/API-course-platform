cda6d3dc60f45f901dbcd8758350198a484e01a5
tiago-ro <tiago.ro.dev@gmail.com>
2024-02-10 12:38:04 -0300

    add controllers and services for categorias, cursos and matriculas

---
+++ src/controllers/MatriculaController.js
@@ -0,0 +1,13 @@
+const Controller = require('./Controller.js');
+const MatriculaServices = require('../services/MatriculaServices.js');
+
+const matriculaServices = new MatriculaServices();
+
+class MatriculaController extends Controller {
+  constructor () {
+    super(matriculaServices);
+  }
+}
+
+module.exports = MatriculaController;
+
